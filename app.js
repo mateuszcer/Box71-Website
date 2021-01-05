66d650fc61ba056f24a56ca54b3e38b361c5bb76
Mateusz Cerulewicz <75334163+mateuszcer@users.noreply.github.com>
2021-01-05 19:08:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+window.onload = () => {
+  setTimeout(() => {
+    document.querySelector("body").classList.add("display");
+  }, 4000);
+};
+
+
 const hamburger = document.querySelector('.hamburger-menu')
 const links = document.querySelector('.links')
 
@@ -5,3 +12,24 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle('menu-active')
     links.classList.toggle('links-active')
 })
+
+// MAKE INPUT FIELD WITH CONTACT FORM DONT COVER TEXT VALUE
+const field = document.querySelectorAll('.field')
+field.forEach((i) => {
+    i.addEventListener('click', (e) => {
+        i.nextSibling.nextSibling.classList.add('clicked');
+})})
+// MAKE LABEL OVER INPUT DEFAULT AGAIN AFTER SUBMITTING FORM
+const submitButton = document.querySelector('.submit-btn')
+submitButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    field.forEach((i) => {
+        i.value = "";
+        i.nextSibling.nextSibling.classList.remove('clicked');
+        i.nextSibling.nextSibling.style.fontSize = '';
+    })
+})
+
+
+
+
